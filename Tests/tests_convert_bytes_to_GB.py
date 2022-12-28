551ca5b643666ab3d59de6ab00ed762f2c832279
ArtemkaKun <zumikq78@gmail.com>
2022-12-28 16:33:51 +0100

    Refactor: test_convert_bytes_to_GB input was simplified

--- Tests/tests_convert_bytes_to_GB.py
+++ Tests/tests_convert_bytes_to_GB.py
@@ -4,4 +4,4 @@ from disk_free_space_checker import convert_bytes_to_GB
 
 
 def test_convert_bytes_to_GB():
-    assert math.isclose(convert_bytes_to_GB(214748364800), 200.0, rel_tol=1e-6) is True
+    assert math.isclose(convert_bytes_to_GB(1073741824), 1.0, rel_tol=1e-6) is True
