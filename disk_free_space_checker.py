b97bdf8773cc7ed03dbd3f0721c9041dce28e1ec
ArtemkaKun <zumikq78@gmail.com>
2022-12-28 13:33:46 +0100

    Implement: arguments handling and main function logic

--- disk_free_space_checker.py
+++ disk_free_space_checker.py
@@ -1,6 +1,7 @@
 import re
 
 import psutil
+import argparse
 
 
 def validate_disks_input(disks_input: list[str]) -> bool:
@@ -20,6 +21,8 @@ def validate_min_space_input(min_free_space: str) -> bool:
 def check_min_free_space_input_bounds(min_free_space: int, disks: list[str]) -> bool:
     for disk in disks:
         if get_disk_total_space_in_GB(disk) <= min_free_space:
+            print(
+                f'Wanted minimum free space is {min_free_space} GB, but disk {disk} has only {get_disk_total_space_in_GB(disk)} GB of total space.')
             return False
 
     return True
@@ -29,5 +32,38 @@ def get_disk_total_space_in_GB(disk: str) -> float:
     return convert_bytes_to_GB(psutil.disk_usage(f'{disk}:\\').total)
 
 
+def get_disk_free_space_in_GB(disk: str) -> float:
+    return convert_bytes_to_GB(psutil.disk_usage(f'{disk}:\\').free)
+
+
 def convert_bytes_to_GB(value_in_bytes: int) -> float:
-    return value_in_bytes / 1024**3
+    return value_in_bytes / 1024 ** 3
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Checks free space on disks')
+    parser.add_argument('--disks', nargs='+', required=True, help='Letters of disks will need be to checked')
+    parser.add_argument('--min-free-space', nargs=1, required=True, help='Minimum free space in GB')
+
+    args = parser.parse_args()
+    disks_input = args.disks
+    min_free_space_input = args.min_free_space[0]
+
+    if validate_disks_input(disks_input) is False:
+        print('Invalid "--disks" input. You must input only letters of disks (for example C D E)')
+        exit(1)
+
+    if validate_min_space_input(min_free_space_input) is False:
+        print('Invalid "--min-free-space" input. You must input only a number that represents space in GB (for example, for 10 GB input 10)')
+        exit(1)
+
+    if check_min_free_space_input_bounds(int(min_free_space_input), disks_input) is False:
+        exit(1)
+
+    for disk in disks_input:
+        free_space = get_disk_free_space_in_GB(disk)
+        if free_space < int(min_free_space_input):
+            print(f'Not enough free space on disk {disk}: {free_space} GB, minimum required: {min_free_space_input} GB')
+            exit(1)
+
+    exit(0)
